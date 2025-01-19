a147556ebd0223d0b7210f334c4a090015dd633e
youjunhui-sky <youjh120608@gmail.com>
2025-01-19 08:40:42 +0800

    add test.vue

---
+++ front-end/h5/src/views/test.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello-world">
+    <h1>Hello World!</h1>
+  </div>
+</template>
+
+<script>
+/**
+ * Hello World 示例页面
+ * @component HelloWorld
+ */
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+.hello-world {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+h1 {
+  font-size: 48px;
+  color: #2c3e50;
+}
+</style>
